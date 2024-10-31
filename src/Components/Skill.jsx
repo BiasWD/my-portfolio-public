c90dbd1fa704e006c25539688424a8181307c40f
BiasWD <134637064+BiasWD@users.noreply.github.com>
2024-10-30 21:03:15 -0500

    Tech Stack Skill

--- src/Components/Skill.jsx
+++ src/Components/Skill.jsx
@@ -21,9 +21,9 @@ function Skill({ logo, title, inView, delay }) {
           src={logo}
           className="h-auto w-full pt-4 opacity-75 transition duration-200 group-hover:opacity-100 object-contain"
         />
-        <span className="font-orbitron pt-1 text-xs leading-tight lg:text-sm">
+        <div className="font-orbitron pt-1 b-auto text-xs leading-tight lg:text-sm">
           {title}
-        </span>
+        </div>
       </div>
     </motion.div>
   );
