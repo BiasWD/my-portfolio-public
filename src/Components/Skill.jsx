a966488a2e02bb5634a5293e22a00599c8cc904f
BiasWD <134637064+BiasWD@users.noreply.github.com>
2024-11-05 12:15:39 -0600

    skill images

--- src/Components/Skill.jsx
+++ src/Components/Skill.jsx
@@ -20,7 +20,7 @@ function Skill({ logo, title, inView, delay }) {
         <div>
         <img
           src={logo}
-          className="h-auto w-full pt-4 opacity-75 transition duration-200 group-hover:opacity-100 object-contain"
+          className="h-12 md:h-20 w-full pt-4 opacity-75 transition duration-200 group-hover:opacity-100 object-contain"
         />
         <span className="font-orbitron pt-1 b-auto text-xs leading-tight lg:text-sm">
           {title}
